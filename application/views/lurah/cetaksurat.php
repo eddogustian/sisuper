d4d96abd9efbe41530330dfb80c8c04a1507710d
eddogustian <eddogustian78@gmail.com>
2020-04-28 17:45:39 +0700

    master

---
+++ application/views/lurah/cetaksurat.php
@@ -0,0 +1,182 @@
+<head>
+  <title>Surat Pengantar Desa JAMBLANG</title>
+  <style type="text/css">
+   .upper { text-transform: uppercase; }
+   .lower { text-transform: lowercase; }
+   .cap   { text-transform: capitalize; }
+   .small { font-variant:   small-caps; }
+</style>
+</head>
+<body bgcolor="white">
+  <form>
+    <table align="center" border="0">
+      <tr>
+        <td rowspan="5"><img src="<?php echo base_url(); ?>asset/images/cirebon.png" alt="Logo Surat" width="140" height="110"></td>
+      </tr>
+      <tr>
+        <td><font face="Times New Roman" size="5"><center>PEMERINTAH KABUPATEN CIREBON</center></font></td>
+      </tr>
+      <tr>
+        <td><font face="Times New Roman" size="5"><center>KECAMATAN JAMBLANG</center></font></td>
+      </tr>
+      <tr>
+        <td><font face="Times New Roman" size="5.5"><center><b>KANTOR KEPALA DESA JAMBLANG</b></center></font></td>
+      </tr>
+      <tr>
+        <td><font face="Times New Roman" size="3"><center>Alamat : JL. Karang Anyar Desa Jamblang Kec. Jamblang Kabupaten Cirebon Pos : 45156</center></font></td>
+      </tr>
+    </table>
+  </form>
+  <hr size="3" noshade="8">
+  <font face="Arial" size="5"> <p align="center"> <u> <b>SURAT KETERANGAN PENGANTAR</b></u></font><br>
+  <font face="Arial" size="4"> Nomor: 8021/DES/2020 </p></font>
+  <form>
+    <table align="center" border="0">
+      <tr>
+        <td align="left" colspan="3">Yang bertanda tangan di bawah ini :</td>
+      </tr>
+      <tr>
+        <td colspan="3">&nbsp;</td>
+      </tr>
+      <tr>
+        <td align="left" width="250">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nama</td>
+        <td>:</td>
+        <td align="left">YOYON KRISTIANTO</td>
+      </tr>
+      <tr>
+        <td align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Jabatan</td>
+        <td>:</td>
+        <td align="left">Kepala Desa Jamblang</td>
+      </tr>
+      <tr>
+        <td align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Alamat</td>
+        <td>:</td>
+        <td align="left">Desa Jamblang RT.04/01 Kec. Jamblang</td>
+      </tr>
+      <tr>
+        <td colspan="3">&nbsp;</td>
+      </tr>
+      <tr>
+        <td align="left" colspan="3">Dengan ini menerangkan bahwa :</td>
+      </tr>
+      <tr>
+        <td colspan="3">&nbsp;</td>
+      </tr>
+      <tr>
+        <td align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nama Lengkap</td>
+        <td>:</td>
+        <td align="left"><?php echo $data['nama']; ?></td>
+      </tr>
+      <tr>
+        <td align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Jenis kelamin</td>
+        <td>:</td>
+        <td align="left"><?php echo $data['jeniskelamin']; ?></td>
+      </tr>
+      <tr>
+        <td align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Agama</td>
+        <td>:</td>
+        <td align="left"><?php echo $data['agama']; ?></td>
+      </tr>
+      <tr>
+        <td align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Status</td>
+        <td>:</td>
+        <td align="left"><?php echo $data['statusperkawinan']; ?></td>
+      </tr>
+      <tr>
+        <td align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;No NIK</td>
+        <td>:</td>
+        <td align="left"><?php echo $data['nik']; ?></td>
+      </tr>
+      <tr>
+        <td align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Tempat/Tanggal lahir</td>
+        <td>:</td>
+        <td align="left"><?php echo $data['tempatlahir']; ?>, <?php echo $data['tanggallahir']; ?></td>
+      </tr>
+      <tr>
+        <td align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pekerjaan</td>
+        <td>:</td>
+        <td align="left"><?php echo $data['jenispekerjaan']; ?></td>
+      </tr>
+      <tr>
+        <td align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Alamat</td>
+        <td>:</td>
+        <td align="left">Desa Jamblang RT <?php echo $data['rt']; ?>/<?php echo $data['rw']; ?> Kec. Jamblang Kabupaten Cirebon</td>
+      </tr>
+      <tr>
+        <td align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Keperluan</td>
+        <td>:</td>
+        <td align="left"><?php echo $data['jenissurat']; ?></td>
+      </tr>
+      <tr>
+        <td align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Keterangan</td>
+        <td>:</td>
+        <td align="left">orang tersebut benar-benar warga Desa Jamblang berkelakuan baik dan belum pernah terkena tindak pidana apapun.</td>
+      </tr>
+      <tr>
+        <td align="left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Berlaku tanggal</td>
+        <td>:</td>
+        <td align="left">Dimulai tanggal <?php echo $data['tanggal']; ?> sampai 30 Hari kedepan.</td>
+      </tr>
+      <tr>
+        <td colspan="3">&nbsp;</td>
+      </tr>
+      <tr>
+        <td align="left" colspan="3">Orang tersebut di atas adalah benar penduduk Desa kami yang berdomisili <b><?php echo $data['statustinggal']; ?></b>, Demikian surat keterangan ini di buat untuk digunakan seperlunya.</td>
+      </tr>
+    </table>
+    <BR>
+    <table align="center" border="0">
+      <tr>
+        <td>
+          Tanda tangan pemegang&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </td>
+        <td>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kepala Desa Jamblang
+        </td>
+      </tr>
+      <td colspan="3">&nbsp;</td>
+    </tr>
+    <tr>
+      <td colspan="3">&nbsp;</td>
+    </tr>
+    <tr>
+      <td colspan="3">&nbsp;</td>
+    </tr>
+    <tr>
+      <td>
+        <font class="upper"><b><i>(<?php echo $data['nama']; ?>)</i></b></font>
+      </td>
+      <td align="right">
+        <font class="upper"><b><i>(YOYON KRISTIANTO)</i></b></font>
+      </td>
+    </tr>
+    <tr>
+      <tr>
+        <td colspan="3">&nbsp;</td>
+      </tr>
+      <tr>
+        <td align="center" colspan="2">
+          Mengetahui :
+        </td>
+      </tr>
+      <tr>
+        <td align="center" colspan="2">
+          Camat Jamblang
+        </tr>
+        <tr>
+          <td colspan="3">&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="3">&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="3">&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2" align="center">
+            <font class="upper"><b><i>(EDDO GUSTIAN, S.KOM)</i></b></font>
+          </td>
+        </tr>
+      </table>
+    </form>
+  </body>
---
+++ application/views/lurah/tentang.php
@@ -0,0 +1,19 @@
+<div class="content-wrapper">
+  <div class="container-fluid">
+    <!-- Breadcrumbs-->
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item">
+        <a href="<?= base_url(''); ?>">Dashboard</a>
+      </li>
+      <li class="breadcrumb-item active">Tentang</li>
+    </ol>
+    <div class="row">
+    	<div class="col">
+    	</div>
+    </div><br>
+    <div align="center">
+    	<font face="consolas" size="7" color="Blue">Aplikasi SISUPER V1.0</font><br>
+        <font face="consolas" size="6">Desa Jamblang</font>
+    </div>
+  </div>
+</div>
